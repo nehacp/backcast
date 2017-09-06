fc15bc2ec72b01c00cd691d0e4de586100d6d439
nehacp <neha.chaudhary@hotmail.com>
2017-09-06 15:38:55 -0700

    Complete VideoListView Tests

--- src/views/videoList.js
+++ src/views/videoList.js
@@ -1,7 +1,16 @@
 var VideoListView = Backbone.View.extend({
 
+  initialize: function() {
+    this.collection.on('sync', this.render, this);
+  },  
 
   render: function() {
+    
+    this.collection.forEach(video => {
+      var videoVideo = new VideoListEntryView({model: video});
+      videoVideo.render();
+    });
+    
     this.$el.children().detach();
     this.$el.html(this.template());
     return this;
